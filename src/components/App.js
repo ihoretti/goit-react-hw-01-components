e33f3e4a8ce960b12b69fbe211e5a3dc271d2915
IHOR <97735057+ihoretti@users.noreply.github.com>
2022-11-28 23:07:59 +0200

    Create App.js

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { StatisticsList } from './Statistics/StatisticsList';
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+
+import { Container } from './App.styled';
+
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
+
+
+export const App = () => {
+  return (
+    <Container>
+      <Profile
+        user={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats">
+        <StatisticsList stats={data} />
+      </Statistics>
+      <FriendList items={friends} />
+      <TransactionHistory transactions={transactions} />
+    </Container>
+  );
+};
+    
+  
+
