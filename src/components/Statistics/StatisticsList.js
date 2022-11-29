62eb79cb1b6da6a482ed49fa686144da335b78e8
IHOR <97735057+ihoretti@users.noreply.github.com>
2022-11-29 23:56:11 +0200

    Update StatisticsList.js

--- src/components/Statistics/StatisticsList.js
+++ src/components/Statistics/StatisticsList.js
@@ -1,22 +1,4 @@
-import PropTypes from 'prop-types';
-import { StatList, Item } from './Statistics.styled';
-import { StatisticsListItem } from './StatisticsListItem';
 
-export const StatisticsList = ({ stats }) => {
-  return (
-    <StatList>
-      {stats.map(doc => (
-        <Item key={doc.id}>
-          <StatisticsListItem doc={doc} />
-        </Item>
-      ))}
-    </StatList>
-  );
-};
-StatisticsList.propTypes = {
-  stats: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-    })
-  ).isRequired,
-};
+
+
+
