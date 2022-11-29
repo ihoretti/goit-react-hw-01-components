9c9ef75fcdd51477db28d4cefc6837e87c9b1883
IHOR <97735057+ihoretti@users.noreply.github.com>
2022-11-30 00:17:48 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
-import { StatisticsList } from './Statistics/StatisticsList';
 import { FriendList } from './FriendList/FriendList';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
@@ -22,9 +21,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics title="Upload stats">
-        <StatisticsList stats={data} />
-      </Statistics>
+      <Statistics title="Upload stats" stats={data} />
       <FriendList items={friends} />
       <TransactionHistory transactions={transactions} />
     </Container>
